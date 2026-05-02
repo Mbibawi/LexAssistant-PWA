d059f897d9f4cd0df4320026140f5b52374ebad3
Mina BIBAWI <minabibawi@gmail.com>
2026-05-02 21:27:33 +0200

    v8.2.9

--- dist/modules/onedrive.js
+++ dist/modules/onedrive.js
@@ -71,7 +71,7 @@ class Configuration {
 class MSAL {
     _clientId = "9cb553c1-8473-4b2a-91d4-fef8b7cd7bff";
     _tenantID = "f45eef0e-ec91-44ae-b371-b160b4bbaa0c";
-    _redirectUri = "https://mbibawi.github.io/LexAssistant-PWA"; //!must be the same domain as the app;
+    _redirectUri = "https://mbibawi.github.io/ExcelInvoicingAddIn"; //!must be the same domain as the app;
     _app = new msal.PublicClientApplication(this.msalConfig()); //!this must come after clientId and redirectUri are delcared
     _initialized = false;
     loginRequest = { scopes: [''] };
